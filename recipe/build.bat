334612ff0e1d5c072d8c87a635eb85fe33387ed2
Uwe L. Korn <uwe.korn@quantco.com>
2025-10-26 16:03:42 +0100

    set PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,3 +1,5 @@
+set PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
+
 %PYTHON% -m pip install . -vv
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ "$PY_VER" == "3.14" ]]; then
+  export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
+fi
+
 $PYTHON -m pip install . -vv
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
